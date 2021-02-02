b02e5c3369b6868a6a7e6a86b588440d974416f2
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2021-02-02 12:14:06 -0500

    Cristian no me quiere

--- App/config.py
+++ App/config.py
@@ -4,4 +4,5 @@ file_path = os.path.join(os.path.dirname(__file__), '..')
 file_dir = os.path.dirname(os.path.realpath('__file__'))
 sys.path.insert(0, os.path.abspath(file_path))
 data_dir = file_dir + '/Data/'
-ff=0
+Cristian=False
+me_la_paso_todo_el_dia_pensandote="Maybe"
